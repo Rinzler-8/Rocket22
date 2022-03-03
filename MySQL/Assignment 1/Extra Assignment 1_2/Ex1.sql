2be9b998e3ae2955a9b9381aa7a536200039f4e8
Rinzler-8 <madboss1801@gmail.com>
2022-03-03 22:45:13 +0700

    Submit assignment2

--- MySQL/Assignment 1/Extra Assignment 1_2/Ex1.sql
+++ MySQL/Assignment 1/Extra Assignment 1_2/Ex1.sql
@@ -1,11 +1,11 @@
 create table Trainee (
-	TraineeID int not null auto_increment,
+    TraineeID int not null auto_increment,
     Full_Name varchar(50) not null,
     Birth_Date date,
     Gender enum("male", "female", "unknown"),
-    ET_IQ tinyint unsigned not null,
-    ET_Gmath tinyint unsigned not null,
-    ET_English tinyint unsigned not null, 
+    ET_IQ   int check(ET_IQ between 0 and  20) not null,
+    ET_Gmath int check(ET_Gmath between 0 and  20) not null, 
+    ET_English  int check(ET_English between 0 and  50) not null, 
     Training_Class varchar(50) not null,
     Evaluation_Notes varchar(50) not null,
     primary key(TraineeID)
